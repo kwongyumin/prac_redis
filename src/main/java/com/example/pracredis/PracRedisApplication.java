8b93d58d25774a48d08a2e673a9ef6ba2bfe5283
kwongyumin <rbals7398@naver.com>
2023-10-04 23:26:12 +0900

    - java 17 빌드 테스트

---
+++ src/main/java/com/example/pracredis/PracRedisApplication.java
@@ -0,0 +1,13 @@
+package com.example.pracredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracRedisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pracredis/PracRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pracredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
