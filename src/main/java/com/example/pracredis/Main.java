9e0374b03b2305614bf6afcece98e1bc79046b76
gwongyumin <rbals7398@naver.com>
2024-01-15 12:17:56 +0900

    - redis(jedis) 테스트, string_type (2)

--- src/main/java/com/example/pracredis/Main.java
+++ src/main/java/com/example/pracredis/Main.java
@@ -2,6 +2,7 @@ package com.example.pracredis;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Pipeline;
 
 import java.util.List;
 
@@ -20,6 +21,24 @@ public class Main {
 
                 List<String> userInfo = jedis.mget("users:300:email","users:300:name","users:300:age");
                 userInfo.forEach(System.out::println);
+
+//                long counter = jedis.incr("counter");
+//                System.out.println(counter);
+//                jedis.incrBy("counter",10L);
+                long counter = jedis.decr("counter");
+                counter = jedis.decrBy("counter",10L);
+                System.out.println(counter);
+
+                // NOTE : redis 파이프라인
+                // SET * n -> SET * 1
+                Pipeline pipeline  = jedis.pipelined();
+                pipeline.set("users:400:email","email.com");
+                pipeline.set("users:400:name","kwon");
+                pipeline.set("users:400:age","15");
+                List<Object> objects = pipeline.syncAndReturnAll();
+                objects.forEach(i -> System.out.println(i.toString()));
+
+
             }
         }
     }
